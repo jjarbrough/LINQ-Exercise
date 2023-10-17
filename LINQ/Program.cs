f3de9caa3a3348e060c8164a74c4648bd1a1c0c7
James Yarbrough <jjarbrough@gmail.com>
2023-10-17 10:26:46 -0500

    finished the first assignment

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace LINQ
+﻿using System.Threading.Channels;
+using System.Linq;
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<String> gameNames = new List<String>();
+            gameNames.Add("The Witcher");
+            gameNames.Add("Baldurs Gate 3");
+            gameNames.Add("CyberPunk 2077");
+            gameNames.Add("Dragonage");
+            gameNames.Add("Mass Effect");
+            gameNames.Add("Call of Duty");
+            gameNames.Add("Apex");
+            gameNames.Add("Borderlands");
+            gameNames.Add("Wonderlands");
+            gameNames.Add("Assassins Creed");
+
+            var temp = gameNames.OrderBy(x => x.Length).ToList();
+            temp.ForEach(x => Console.WriteLine(x));
         }
     }
 }
